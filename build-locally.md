4422b5aa197c567affc59aae30b12a31cfc98d6c
sam <sam.owen.consultant@springer.com>
2018-11-23 15:52:07 +0000

    build instructions

--- build-locally.md
+++ build-locally.md
@@ -1,10 +1,16 @@
 For local development
 
 * clone the squirrel foe
-git@github.com:kiwipower/squirrel-lang-idea-plugin.git
+```git@github.com:kiwipower/squirrel-lang-idea-plugin.git```
 
 * download intellij comminuty edition, and extract somewhere
-https://www.jetbrains.com/idea/download
+```https://www.jetbrains.com/idea/download```
 
-*
+* download intellij comminity edition sources to idea in theis project
+```git clone --depth 1 git@github.com:JetBrains/intellij-community.git idea```
 
+* open project structure. Delete the SDK and re-add pointing to the community edition home directory. In sourcepath click + and add the idea directorty from above.
+
+* run the 'build.sh' file
+
+* compile all
--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 rm -rf gen
 java -jar tools/grammar-kit.jar gen src/com/sqide/Squirrel.bnf
-java -jar idea/tools/lexer/jflex-1.7.0.jar --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelLexer.flex -d gen/com/sqide/lexer/
-java -jar idea/tools/lexer/jflex-1.7.0.jar --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelDocLexer.flex -d gen/com/sqide/lexer/
+java -cp 'tools/*' jflex.Main --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelLexer.flex -d gen/com/sqide/lexer/
+java -cp 'tools/*' jflex.Main --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelDocLexer.flex -d gen/com/sqide/lexer/
+                                    
