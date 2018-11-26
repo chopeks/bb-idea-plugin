e551ee6f1b68c925c5aec7340271530395564cde
sam <sam.owen.consultant@springer.com>
2018-11-26 17:00:35 +0000

    move idea flex to tools

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ java -cp "tools/grammar-kit.jar" org.intellij.grammar.Main gen src/com/sqide/Squ
 
 ./gradlew clean compileJava
 
-java -cp 'tools/*' jflex.Main --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelLexer.flex -d gen/com/sqide/lexer/
-java -cp 'tools/*' jflex.Main --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelDocLexer.flex -d gen/com/sqide/lexer/
+java -cp 'tools/*' jflex.Main --skel tools/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelLexer.flex -d gen/com/sqide/lexer/
+java -cp 'tools/*' jflex.Main --skel tools/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelDocLexer.flex -d gen/com/sqide/lexer/
 
 java -cp "build/classes/java/main:tools/grammar-kit.jar" org.intellij.grammar.Main gen src/com/sqide/Squirrel.bnf
 
