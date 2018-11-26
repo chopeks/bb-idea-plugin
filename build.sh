a07640b850a04d5d2e5a592129a48ca7f71aa096
sam <sam.owen.consultant@springer.com>
2018-11-26 15:16:13 +0000

    wow - it builds

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 rm -rf gen
-java -jar tools/grammar-kit.jar gen src/com/sqide/Squirrel.bnf
+java -cp "tools/grammar-kit.jar" org.intellij.grammar.Main gen src/com/sqide/Squirrel.bnf
+
+./gradlew clean compileJava
+
 java -cp 'tools/*' jflex.Main --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelLexer.flex -d gen/com/sqide/lexer/
 java -cp 'tools/*' jflex.Main --skel idea/tools/lexer/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelDocLexer.flex -d gen/com/sqide/lexer/
-                                    
+
+java -cp "build/classes/java/main:tools/grammar-kit.jar" org.intellij.grammar.Main gen src/com/sqide/Squirrel.bnf
+
+./gradlew clean buildPlugin
