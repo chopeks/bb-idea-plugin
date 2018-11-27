24f6c20568161d5357d6e3b6fc058c383883d271
sam <sam.owen.consultant@springer.com>
2018-11-27 11:41:51 +0000

    download idea in script - gradle seems to hang

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "idea" ]; then
+    curl https://cache-redirector.jetbrains.com/www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/ideaIC/2018.3/ideaIC-2018.3.zip
+    unzip ideaIC-2018.3.zip -d idea
+fi
+
 rm -rf gen
 java -cp "tools/grammar-kit.jar" org.intellij.grammar.Main gen src/com/sqide/Squirrel.bnf
 
