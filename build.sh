de0193315f0c5499794288809738ca41df9eab59
sam <sam.owen.consultant@springer.com>
2018-11-27 12:04:13 +0000

    move compile a bit further down

--- build.sh
+++ build.sh
@@ -12,11 +12,10 @@ fi
 rm -rf gen
 java -cp "tools/grammar-kit.jar" org.intellij.grammar.Main gen src/com/sqide/Squirrel.bnf
 
-./gradlew clean compileJava
-
 java -cp 'tools/*' jflex.Main --skel tools/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelLexer.flex -d gen/com/sqide/lexer/
 java -cp 'tools/*' jflex.Main --skel tools/idea-flex.skeleton --nobak src/com/sqide/lexer/SquirrelDocLexer.flex -d gen/com/sqide/lexer/
 
+./gradlew clean compileJava
 java -cp "build/classes/java/main:tools/grammar-kit.jar" org.intellij.grammar.Main gen src/com/sqide/Squirrel.bnf
 
 ./gradlew clean buildPlugin
